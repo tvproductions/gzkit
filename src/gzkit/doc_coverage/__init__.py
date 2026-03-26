f1fd453647ac701d10f5b89524529cbfa07140b2
Jeff <test@example.com>
2026-03-26 06:07:21 -0500

    chore: update config, data, docs/design/adr (2 files), doc_coverage (2 files) +1 more (gz git-sync)

--- src/gzkit/doc_coverage/__init__.py
+++ src/gzkit/doc_coverage/__init__.py
@@ -1,13 +1,25 @@
 """Documentation cross-coverage scanner for CLI commands."""
 
+from gzkit.doc_coverage.manifest import (
+    CommandEntry,
+    DocCoverageManifest,
+    SurfaceRequirements,
+    find_undeclared_commands,
+    load_manifest,
+)
 from gzkit.doc_coverage.models import CommandCoverage, CoverageReport, OrphanedDoc, SurfaceResult
 from gzkit.doc_coverage.scanner import check_surfaces_report, scan_cli_commands
 
 __all__ = [
     "CommandCoverage",
+    "CommandEntry",
     "CoverageReport",
+    "DocCoverageManifest",
     "OrphanedDoc",
+    "SurfaceRequirements",
     "SurfaceResult",
     "check_surfaces_report",
+    "find_undeclared_commands",
+    "load_manifest",
     "scan_cli_commands",
 ]
